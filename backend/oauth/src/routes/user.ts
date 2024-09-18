d9b45203a49fbf78ebc19f3e2d658c0834354434
ConradoCrestani <conradoacrestani@gmail.com>
2024-09-18 19:27:35 -0300

    deleteUser

--- backend/oauth/src/routes/user.ts
+++ backend/oauth/src/routes/user.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getUser, getUserById, createUser } from '../controllers/userController.js'
+import { getUser, getUserById, createUser, deleteUser } from '../controllers/userController.js'
 import { validateRequest } from '../middlewares/validateRequest.js'
 import { createUserSchema } from '../schemas/userSchema.js'
 
@@ -8,5 +8,6 @@ const router = express.Router()
 router.get('/', getUser)
 router.get('/:id', getUserById)
 router.post('/', validateRequest(createUserSchema), createUser)
+router.delete('/:id', deleteUser)
 
 export default router
