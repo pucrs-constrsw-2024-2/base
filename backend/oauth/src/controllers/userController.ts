5da23702059b58321b3489f228cb79127d3a4163
Eduarda Bertiz <eduardabertiz@hotmail.com>
2024-09-23 20:09:08 -0300

    Funcionalidade PUT

--- backend/oauth/src/controllers/userController.ts
+++ backend/oauth/src/controllers/userController.ts
@@ -60,6 +60,8 @@ export const createUser = async (req: Request, res: Response) => {
 
   try {
     const userData = req.body as z.infer<typeof createUserSchema>
+    console.log(userData);
+    
 
     const response = await fetch(
       `${process.env.KEYCLOAK_URL}/admin/realms/${process.env.KEYCLOAK_REALM}/users`,
@@ -83,4 +85,47 @@ export const createUser = async (req: Request, res: Response) => {
     console.error('Error:', error)
     res.status(500).json({ error: 'Internal Server Error' })
   }
+}
+
+export const updateUser = async (req: Request, res: Response) => {
+   const token = req.headers.authorization?.split(' ')[1]
+   const userId = req.params.id
+
+   console.log(userId);
+
+   
+
+  if (!token) {
+     return res.status(401).json({ error: 'Unauthorized' })
+ }
+
+try {
+     const updatedData = req.body
+console.log(updatedData);
+
+     const response = await fetch(
+      `${process.env.KEYCLOAK_URL}/admin/realms/${process.env.KEYCLOAK_REALM}/users/${userId}`,
+      {
+         method: 'PUT',
+headers: {
+         Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+         body: JSON.stringify(updatedData),
+     }
+     )
+   if (!response.ok) {
+       const errorData = await response.json()
+       return res.status(response.status).json(errorData)
+  }
+
+  console.log(response);
+  
+
+ res.status(200).json({ message: 'User updated successfully' })
+  } catch (error) {
+    console.error('Error:', error)
+     res.status(500).json({ error: 'Internal Server Error' })
+
+}
 }
--- backend/oauth/src/routes/user.ts
+++ backend/oauth/src/routes/user.ts
@@ -1,12 +1,13 @@
 import express from 'express'
-import { getUser, getUserById, createUser } from '../controllers/userController.js'
+import { getUser, getUserById, createUser, updateUser } from '../controllers/userController.js'
 import { validateRequest } from '../middlewares/validateRequest.js'
-import { createUserSchema } from '../schemas/userSchema.js'
+import { createUserSchema, updateUserSchema } from '../schemas/userSchema.js'
 
 const router = express.Router()
 
 router.get('/', getUser)
 router.get('/:id', getUserById)
 router.post('/', validateRequest(createUserSchema), createUser)
+router.put('/:id', validateRequest(updateUserSchema), updateUser)
 
 export default router
