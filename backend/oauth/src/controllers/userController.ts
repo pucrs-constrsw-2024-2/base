ad7a6d72013cabdebe1a5b43253215df35031801
MarcusQuirino <marcusfilipus@gmail.com>
2024-09-25 19:05:13 -0300

    dockerize application

--- backend/oauth/src/controllers/userController.ts
+++ backend/oauth/src/controllers/userController.ts
@@ -1,4 +1,4 @@
-import { passwordSchema } from './../schemas/passwordSchema.js';
+import { passwordSchema } from './../schemas/passwordSchema.js'
 import { Request, Response } from 'express'
 import { z } from 'zod'
 import { createUserSchema } from '../schemas/userSchema.js'
@@ -61,8 +61,7 @@ export const createUser = async (req: Request, res: Response) => {
 
   try {
     const userData = req.body as z.infer<typeof createUserSchema>
-    console.log(userData);
-    
+    console.log(userData)
 
     const response = await fetch(
       `${process.env.KEYCLOAK_URL}/admin/realms/${process.env.KEYCLOAK_REALM}/users`,
@@ -81,7 +80,7 @@ export const createUser = async (req: Request, res: Response) => {
       return res.status(response.status).json(errorData)
     }
 
-    res.status(201).json({ message: 'User created successfully' })
+    res.status(204).send()
   } catch (error) {
     console.error('Error:', error)
     res.status(500).json({ error: 'Internal Server Error' })
@@ -89,46 +88,42 @@ export const createUser = async (req: Request, res: Response) => {
 }
 
 export const updateUser = async (req: Request, res: Response) => {
-   const token = req.headers.authorization?.split(' ')[1]
-   const userId = req.params.id
+  const token = req.headers.authorization?.split(' ')[1]
+  const userId = req.params.id
 
-   console.log(userId);
-
-   
+  console.log(userId)
 
   if (!token) {
-     return res.status(401).json({ error: 'Unauthorized' })
- }
-
-try {
-     const updatedData = req.body
-console.log(updatedData);
-
-     const response = await fetch(
-      `${process.env.KEYCLOAK_URL}/admin/realms/${process.env.KEYCLOAK_REALM}/users/${userId}`,
-      {
-         method: 'PUT',
-headers: {
-         Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-         body: JSON.stringify(updatedData),
-     }
-     )
-   if (!response.ok) {
-       const errorData = await response.json()
-       return res.status(response.status).json(errorData)
+    return res.status(401).json({ error: 'Unauthorized' })
   }
 
-  console.log(response);
-  
+  try {
+    const updatedData = req.body
+    console.log(updatedData)
 
- res.status(200).json({ message: 'User updated successfully' })
+    const response = await fetch(
+      `${process.env.KEYCLOAK_URL}/admin/realms/${process.env.KEYCLOAK_REALM}/users/${userId}`,
+      {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedData),
+      },
+    )
+    if (!response.ok) {
+      const errorData = await response.json()
+      return res.status(response.status).json(errorData)
+    }
+
+    console.log(response)
+
+    res.status(204).send()
   } catch (error) {
     console.error('Error:', error)
-     res.status(500).json({ error: 'Internal Server Error' })
-
-}
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
 }
 
 export const deleteUser = async (req: Request, res: Response) => {
@@ -138,7 +133,9 @@ export const deleteUser = async (req: Request, res: Response) => {
   if (!token) {
     return res.status(401).json({ error: 'Unauthorized' })
   }
-      const response = await fetch(
+
+  try {
+    const response = await fetch(
       `${process.env.KEYCLOAK_URL}/admin/realms/${process.env.KEYCLOAK_REALM}/users/${userId}`,
       {
         method: 'DELETE',
@@ -153,45 +150,42 @@ export const deleteUser = async (req: Request, res: Response) => {
       return res.status(response.status).json(errorData)
     }
 
-    res.status(204).json({ message: 'User deleted successfully' })
+    res.status(204).send()
   } catch (error) {
     console.error('Error:', error)
     res.status(500).json({ error: 'Internal Server Error' })
   }
 }
-  
- export const updatePassword = async (req: Request, res: Response) => {
-  const token = req.headers.authorization?.split(' ')[1];
-  const userId = req.params.id;
+
+export const updatePassword = async (req: Request, res: Response) => {
+  const token = req.headers.authorization?.split(' ')[1]
+  const userId = req.params.id
 
   try {
-    const passwordData = req.body as z.infer <typeof passwordSchema>
+    const passwordData = req.body as z.infer<typeof passwordSchema>
     const payload = {
       value: passwordData.password,
       type: 'password',
-      temporary: false
+      temporary: false,
     }
     const response = await fetch(
-    `${process.env.KEYCLOAK_URL}/admin/realms/${process.env.KEYCLOAK_REALM}/users/${userId}/reset-password`,
-    {
-      method: 'PUT',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json',
+      `${process.env.KEYCLOAK_URL}/admin/realms/${process.env.KEYCLOAK_REALM}/users/${userId}/reset-password`,
+      {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
       },
-      body: JSON.stringify(payload),
-    },
-  )
-  if (!response.ok) {
-    const errorData = await response.json()
-    return res.status(response.status).json(errorData)
+    )
+    if (!response.ok) {
+      const errorData = await response.json()
+      return res.status(response.status).json(errorData)
     }
-  res.status(204).send();
-  }
-  catch(error) {
+    res.status(204).send()
+  } catch (error) {
     console.error('Error:', error)
     res.status(500).json({ error: 'Internal Server Error' })
-
   }
 }
-
