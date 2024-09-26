af6bc04df61924d4429be9544d85c2fbbb1bab84
gfgiaretta <gabriel.giaretta001@edu.pucrs.br>
2024-09-25 23:56:35 -0300

    add: Swagger

--- backend/utils/oauth/app.js
+++ backend/utils/oauth/app.js
@@ -1,20 +1,29 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const authRoutes = require('./src/routes/users')
-const { urlencoded } = require('body-parser')
-const bodyParser = require('body-parser')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./src/routes/users');
+const { urlencoded } = require('body-parser');
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const yaml = require('js-yaml');
 
-const app = express()
-const PORT = process.env.OAUTH_INTERNAL_PORT || 3000
+const swaggerDocument = yaml.load(fs.readFileSync('./swagger.yaml', 'utf8'));
+
+
+const app = express();
+const PORT = process.env.OAUTH_INTERNAL_PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(urlencoded({extended: true}));
+app.use(urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/auth', authRoutes)
+// Configuração do Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () =>
     console.log('rodando')
-)
+);
