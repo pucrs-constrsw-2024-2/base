185471744c9ed0d3ccc223f4d2e21f1d4133e16e
fernanda.mello <fernanda.mello02@edu.pucrs.br>
2024-10-09 19:48:16 -0300

    Adiciona descricoes das operacoes de GroupsController

--- backend/oauth/src/main/java/Group7/OAuth/adapter/controllers/GroupsController.java
+++ backend/oauth/src/main/java/Group7/OAuth/adapter/controllers/GroupsController.java
@@ -5,6 +5,7 @@ import Group7.OAuth.application.usecase.AddUserGroupUC;
 import Group7.OAuth.application.usecase.DeleteUserGroupUC;
 import Group7.OAuth.application.usecase.GetGroupsUC;
 import Group7.OAuth.application.usecase.GetUserGroupsUC;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -31,12 +32,14 @@ public class GroupsController {
     private final DeleteUserGroupUC deleteUserGroupUC;
 
     @GetMapping
+    @Operation(summary = "Recuperação dos dados de todos os grupos cadastrados")
     public ResponseEntity<Collection<GroupDTO>> getGroups(@AuthenticationPrincipal Jwt jwt) {
         String token = jwt.getTokenValue();
         return ResponseEntity.ok(getGroupsUC.run(token));
     }
 
     @PutMapping(path = "/users/{id}")
+    @Operation(summary = "Adição do usuário em um grupo")
     public ResponseEntity<Void> addUserGroup(@PathVariable UUID id, @RequestBody String group,
                                              @AuthenticationPrincipal Jwt jwt){
         addUserGroupUC.run(jwt.getTokenValue(), id, group);
@@ -44,6 +47,7 @@ public class GroupsController {
     }
 
     @DeleteMapping(path = "/users/{id}")
+    @Operation(summary = "Exclusão do usuário em um grupo")
     public ResponseEntity<Void> deleteUserGroup(@PathVariable UUID id, @RequestBody String group,
                                                 @AuthenticationPrincipal Jwt jwt){
         deleteUserGroupUC.run(jwt.getTokenValue(), id, group);
@@ -52,6 +56,7 @@ public class GroupsController {
 
     //get user groups
     @GetMapping(path = "/users/{id}")
+    @Operation(summary = "Recuperação dos grupos aos quais o usuário pertence")
     public ResponseEntity<Collection<GroupDTO>> getUserGroups(@PathVariable UUID id, @AuthenticationPrincipal Jwt jwt){
         return ResponseEntity.ok(getUserGroupsUC.run(jwt.getTokenValue(), id));
     }
