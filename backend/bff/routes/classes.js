730f05d979e2a0f41747ef078c5b83e308176271
Eduardo Arruda <13435770+eduardo-arruda@users.noreply.github.com>
2024-11-13 20:13:52 -0300

    Ajusta BFF e compose

--- backend/bff/routes/classes.js
+++ backend/bff/routes/classes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { createClass, deleteClass, getClasses, getClassesByQuery, putClass, patchClass } = require('../controllers/classesController');
 
-router.post('/classes/', createClass);
-router.get('/classes/', getClasses);
-router.get('/classes/:id', getClasses);
-router.get('/classes/query', getClassesByQuery);
-router.put('/classes/:id', putClass);
-router.patch('/classes/:id', patchClass);
-router.delete('/classes/:id', deleteClass);
+router.post('/', createClass);
+router.get('/', getClasses);
+router.get('/:id', getClasses);
+router.get('/query', getClassesByQuery);
+router.put('/:id', putClass);
+router.patch('/:id', patchClass);
+router.delete('/:id', deleteClass);
 
 module.exports = router;
