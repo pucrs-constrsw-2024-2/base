ad1c36b44a4853c3d428d247adc40c442314c242
Hartur Kunzler <hartur.kunzler@gmail.com>
2024-11-13 21:12:17 -0300

    Roda bff

--- backend/bff/routes/resources.js
+++ backend/bff/routes/resources.js
@@ -1,12 +1,19 @@
-const express = require('express');
-const { createResource, deleteResourceById, fullResourceUpdate, getAllResources, getResourceById, partialResourceUpdate} = require('../controllers/ResourcesController');
+const express = require("express");
+const {
+	createResource,
+	deleteResourceById,
+	fullResourceUpdate,
+	getAllResources,
+	getResourceById,
+	partialResourceUpdate,
+} = require("../controllers/resourcesController");
 const router = express.Router();
 
-router.post('/', createResource);
-router.get('/', getAllResources);
-router.get('/:id', getResourceById);
-router.put('/:id', fullResourceUpdate);
-router.patch('/:id', partialResourceUpdate);
-router.delete('/:id', deleteResourceById);
+router.post("/", createResource);
+router.get("/", getAllResources);
+router.get("/:id", getResourceById);
+router.put("/:id", fullResourceUpdate);
+router.patch("/:id", partialResourceUpdate);
+router.delete("/:id", deleteResourceById);
 
 module.exports = router;
